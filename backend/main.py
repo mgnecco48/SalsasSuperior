6da3599a633f084b0663874eb4ed2835769f5d90
Martin Gnecco <mgnecco48@gmail.com>
2026-07-22 00:40:38 +0200

    Changed config for vite to be reachable remotely

--- backend/main.py
+++ backend/main.py
@@ -79,7 +79,7 @@ app.mount("/media", StaticFiles(directory="media"), name="media")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    server: {
+        port: 3000,
+        open: true,
+        host: '0.0.0.0',
+        allowedHosts: ['localhost', 'omarchy'],
+    }
 })
